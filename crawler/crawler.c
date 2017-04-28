f8a42d878a72923403b8feda470858fecc3832f7
Savannah Liu <savannah.m.liu.18@dartmouth.edu>
2017-04-28 11:41:02 -0400

    added crawler.c: works up to depth 2)

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -23,20 +23,18 @@
 #include "hashtable.h"
 #include "webpage.h"
 
-// webpage items that go into bag: contains url and depth
-struct page {
-  char *url;
-  int depth;
-}
 
 const int HT_NUM_SLOTS = 100;
-const char * HT_ITEM = "ht item"; // item stored in ht
+char * HT_ITEM = "htitem"; // item stored in ht
 
 /* function prototypes */
 void crawler(char *seedURL, char *pageDirectory, int maxDepth);
-//webpage_t *pagefetcher(char *url, const int depth, char *html);
-//void pagesaver();
-//void pagescanner(hashtable, bag, URL, depth);
+bool pagefetcher(webpage_t *page);
+void pagesaver(webpage_t *page, int id, char *pageDirectory);
+void pagescanner(webpage_t *page, hashtable_t *ht, bag_t *bag);
+
+//static void bagitemprint(FILE *fp, void *item);
+//static void htitemprint(FILE *fp, const char *key, void *item);
 
 /* main function
 * parses arguments and initializes other modules
@@ -59,8 +57,8 @@ int main (const int argc, char *argv[]) {
   // correct number of arguments
   seedURL = argv[1];
   pageDirectory = argv[2];
-  char *filename = count_malloc(sizeof(char *));;  // used for checking pageDirectory
-  strcpy(filename, pageDirectory);
+  char *filename = count_malloc(sizeof(char *));  // used for checking pageDirectory
+  strcpy(filename, pageDirectory); // used for checking pageDirectory
   maxDepthString = argv[3];
 
   // check URL is internal aka within CS50 playground http://old-www.cs.dartmouth.edu/
@@ -85,11 +83,10 @@ int main (const int argc, char *argv[]) {
     fprintf(stderr, "usage: maxDepth must be a non-negative integer\n");
     exit(4);
   }
-
   count_free(filename);
 
   // initialize other modules  --------------------------------------------
-
+  crawler(seedURL, pageDirectory, maxDepth);
 }
 
 /* Loops over pages to explore until list is exhausted
@@ -98,49 +95,109 @@ int main (const int argc, char *argv[]) {
  * then the result to the pagesaver, then to the pagescanner
 */
 void crawler(char *seedURL, char *pageDirectory, int maxDepth){
-  // create bag of webpages to crawl
-  bag_t *bag;
+  bag_t *bag;   // create bag of webpages to crawl
   bag = bag_new();
   if (bag == NULL) {
     fprintf(stderr, "bag_new failed\n");
     exit(5);
   }
 
-  // create hashtable of webpages seen
-  hashtable_t *ht;
+  hashtable_t *ht;   // create hashtable of webpages seen
   ht = hashtable_new(HT_NUM_SLOTS);
   if (ht == NULL) {
     fprintf(stderr, "hashtable_new failed\n");
     exit(6);
   }
 
-  // create first page struct for seedURL
-  struct page *pg = count_malloc(sizeof(struct page));
-  pg->url = seedURL;
-  pg->depth = 0;
-  bag_insert(bag, pg);
-  hashtable_insert(ht, pg->URL, HT_ITEM);
+  webpage_t *page;  // create first page struct for seedURL
+  page = webpage_new(seedURL, 0, NULL);
+  bag_insert(bag, page);
+  hashtable_insert(ht, seedURL, HT_ITEM);
 
-  while (pg = bag_extract(bag) != NULL){ // while bag is not empty: there are more webpages to crawl
-    char * url = pg->url;
+  int id = 1;
+  while ((page = bag_extract(bag)) != NULL) { // while bag is not empty: there are more webpages to crawl
 
-    
+    if (pagefetcher(page) == true) { // pause for a second & use pagefetcher to get a webpage for the URL
+      pagesaver(page, id, pageDirectory); // write webpage to pageDirectory with unique doc id
+      id++; // update id for next document
+      if (webpage_getDepth(page) < maxDepth){ // if webpage depth is less than maxdepth, explore the webpage for links
+        pagescanner(page, ht, bag);
+      }
+    }
+  }
+}
 
+/* fetches the HTML of a page from a URL*/
+bool pagefetcher(webpage_t *page){
+  return webpage_fetch(page);
+}
+
+//outputs a page to the appropriate file: uses pagefetcher contents
+void pagesaver(webpage_t *page, int id, char *pageDirectory){
+  char *newfile = count_malloc(sizeof(char *));
+  //char *filename = count_malloc(sizeof(char *));  // used for checking pageDirectory
+  strcpy(newfile, pageDirectory); // used for checking pageDirectory
+
+  //newfile = "hello";
+  //strcat(filename, fileEnding); // concatenate string https://www.tutorialspoint.com/c_standard_library/c_function_strcat.htm
+
+  sprintf(newfile, "%s%d", newfile, id); // creates add id to filename by appending to pageDirectory
+
+  FILE *fp;
+  fp = fopen(newfile, "w");
+  fprintf(fp, "%s\n %d\n", webpage_getURL(page), webpage_getDepth(page)); // write URL and depth to the file
+  fprintf(fp, "%s", webpage_getHTML(page));
+  fclose(fp); // close the file
+  free(newfile);
+
+/*
+  sprintf(pageDirectory, "%s%d", pageDirectory, id); // creates add id to filename by appending to pageDirectory
+  FILE *fp;
+  fp = fopen(pageDirectory, "w");
+  fprintf(fp, "%s\n %d\n", webpage_getURL(page), webpage_getDepth(page)); // write URL and depth to the file
+  fprintf(fp, "%s", webpage_getHTML(page));
+  fclose(fp); // close the file
+  */
+}
+
+
+/* extracts URLs from a page and processes each one*/
+void pagescanner(webpage_t *page, hashtable_t *ht, bag_t *bag){
+  // parse webpage to extract all URLS
+  int depth = webpage_getDepth(page);
+  depth++;
+  int pos = 0;   // position in the HTML
+  char *result;  // URL retreived
+
+  // write the html of the webpage to the file in pageDirectory
+  while ((pos = webpage_getNextURL(page, pos, &result)) > 0){ // while you have not run out of HTML
+    // store next URL in the html into result. update position
+    if (NormalizeURL(result) == true) { // normalize the url
+      if (IsInternalURL(result) == true){ // ignore non-internal links
+        if (hashtable_insert(ht, result, HT_ITEM) == true){ // insert into ht
+          webpage_t *page; // make a new webpage at depth + 1 if inserted
+          page = webpage_new(result, depth, NULL);
+          bag_insert(bag, page);
+        }
+      }
+    }
+    free(result);
   }
 }
 
 
-/* fetches the HTML of a page from a URL
-webpage_t *pagefetcher(char *url, const int depth, char *html){
-}
-*/
+// Testing functions -----------------------------------------------------
 
-/* outputs a page to the appropriate file: uses pagefetcher contents
-void pagesaver(char *url, const int depth, html from pagefetcher?, const *filename){
+/*Prints out a webpage bag item (url, depth, html) for testing purposes.
+static void bagitemprint(FILE *fp, void *item){
+  if (fp != NULL && item != NULL)
+    fprintf(fp, "page: url %s, depth %d\n", webpage_getURL(item), webpage_getDepth(item));
 }
-*/
 
-/* extracts URLs from a page and returns one at a time ???
-webpage_t *pagefetcher(html from pagefetcher?){
+Prints out a webpage ht item (url, depth, html) for testing purposes.
+static void htitemprint(FILE *fp, const char *key, void *item){
+  char *string = item;
+  if (fp != NULL && key != NULL && item != NULL)
+  fprintf(fp, "URL key: %s, const string item %s\n", key, string);
 }
 */
